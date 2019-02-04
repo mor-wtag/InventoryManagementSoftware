7016710c97213fd13ff69093081f89ecac0395dd
Ramisa Ibnat <ramisa.ib@gmail.com>
2019-02-04 17:21:07 +0600

    Added Loading page while Authenticating.

--- Assets/app.js
+++ Assets/app.js
@@ -1,5 +1,9 @@
 
 console.log("Initiallizing Firebase...");
+
+//hide loading function
+$('.load-4').hide();
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAPITJ_b82lngDCMBkqOP4sf28fogy_QMc",
@@ -14,6 +18,46 @@ var initialize = firebase.initializeApp(config);
 if (initialize) {
     console.log("Firebase initialized");
 }
+
+
+
+// RealTime listener
+firebase.auth().onAuthStateChanged( firebaseUser => {
+    if (firebaseUser){
+        Current_UID = firebaseUser.uid;
+        console.log("Already Signed in...");
+
+    }
+    else{
+        console.log('Not logged in...');
+    }
+});
+
+$('input[type="submit"]').click(function(){
+    $('.login').fadeOut(100);
+    $('.load-4').fadeIn(500, function(){
+        $('.load-4').fadeOut(1000, function(){
+            $('.login').fadeIn(500);
+        });
+    });
+        // $('.login').fadeIn("slow");6000
+    // });
+
+    // setTimeout(function(){
+    //   $('.load-4').css('display','inline-block').fadeIn();},300);
+    // setTimeout(function(){
+    //   $(".authent").show();
+    // //   $(".authent").animate({opacity: 1},{duration: 200, queue: false }).addClass('visible');
+    // },500);
+    // setTimeout(function(){
+    //   $(".authent").show().animate({right:90},{easing : 'easeOutQuint' ,duration: 600, queue: false });
+    //   $(".authent").animate({opacity: 0},{duration: 200, queue: false }).addClass('visible');
+    //   $('.login').removeClass('testtwo')
+    // },2500);
+    // setTimeout(function(){
+    //   $('.login').removeClass('test')
+    //   $('.login div').fadeOut(123);
+    // },2800);
 
 // get all elements
 
@@ -38,14 +82,26 @@ $('#loginbtn').click(function () {
     const auth = firebase.auth();
 
     //   Sign In
-    const promise = auth.signInWithEmailAndPassword(email, password);
-    if (promise) {
-        console.log("Successfully Signed in...");
+    const promise = auth.signInWithEmailAndPassword(email, password).then(function(user){
+        Current_UID = user.uid;
+        console.log("Logged in successfully...");
+    }, function(err) {
+        console.log(err.code);
+        $("#Login_After_Title").html('Authentication Failed');
+        Error_String = err.code.substr(5);
+        $("#Login_After_Para").html('Error ID: ' + Error_String) ;
+    });
 
-    }
-    else {
-        alert("Worng Username/ Password. Please Try again.");
-        promise.catch(e => console.log(e.message));
-    }
+    
+    // if (!promise) {
+    //     alert("Worng Username/ Password. Please Try again.");
+    //     promise.catch(e => console.log(e.message));
+    //     }
+    // else {
+        
+    //     window.location.href = "User_page.html";
+    
+        
+    });
 
 });
