3073068a4d3e477e19303f1e6da11867d9e06f0d
Ramisa Ibnat <ramisa.ib@gmail.com>
2019-04-07 15:03:12 +0600

    implemented date range to obtain data from database in Entry log page

--- Inventory Management Software Volume 2/src/entry_log.js
+++ Inventory Management Software Volume 2/src/entry_log.js
@@ -282,5 +282,270 @@ function initialLoad(){
     
             }
         });
+
+        //PICKING DATE RANGE
+        //This code here to select a date range according to which the data will be viewed later
+
+        (function($) {
+            function compareDates(startDate, endDate, format) {
+              var temp, dateStart, dateEnd;
+              try {
+                dateStart = $.datepicker.parseDate(format, startDate);
+                dateEnd = $.datepicker.parseDate(format, endDate);
+                if (dateEnd < dateStart) {
+                  temp = startDate;
+                  startDate = endDate;
+                  endDate = temp;
+                }
+              } catch (ex) {}
+              return { start: startDate, end: endDate };
+            }
+          
+            $.fn.dateRangePicker = function (options) {
+              options = $.extend({
+                "changeMonth": false,
+                "changeYear": false,
+                "numberOfMonths": 2,
+                "rangeSeparator": " - ",
+                      "useHiddenAltFields": false
+              }, options || {});
+          
+                  var myDateRangeTarget = $(this);
+              var onSelect = options.onSelect || $.noop;
+              var onClose = options.onClose || $.noop;
+              var beforeShow = options.beforeShow || $.noop;
+              var beforeShowDay = options.beforeShowDay;
+              var lastDateRange;
+          
+              function storePreviousDateRange(dateText, dateFormat) {
+                var start, end;
+                dateText = dateText.split(options.rangeSeparator);
+                if (dateText.length > 0) {
+                  start = $.datepicker.parseDate(dateFormat, dateText[0]);
+                  if (dateText.length > 1) {
+                    end = $.datepicker.parseDate(dateFormat, dateText[1]);
+                  }
+                  lastDateRange = {start: start, end: end};
+                } else {
+                  lastDateRange = null;
+                }
+              }
+                  
+              options.beforeShow = function(input, inst) {
+                var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
+                storePreviousDateRange($(input).val(), dateFormat);
+                beforeShow.apply(myDateRangeTarget, arguments);
+              };
+                  
+              options.beforeShowDay = function(date) {
+                var out = [true, ""], extraOut;
+                if (lastDateRange && lastDateRange.start <= date) {
+                  if (lastDateRange.end && date <= lastDateRange.end) {
+                    out[1] = "ui-datepicker-range";
+                  }
+                }
+          
+                if (beforeShowDay) {
+                  extraOut = beforeShowDay.apply(myDateRangeTarget, arguments);
+                  out[0] = out[0] && extraOut[0];
+                  out[1] = out[1] + " " + extraOut[1];
+                  out[2] = extraOut[2];
+                }
+                return out;
+              };
+          
+              options.onSelect = function(dateText, inst) {
+                var textStart;
+                if (!inst.rangeStart) {
+                  inst.inline = true;
+                  inst.rangeStart = dateText;
+                } else {
+                  inst.inline = false;
+                  textStart = inst.rangeStart;
+                  if (textStart !== dateText) {
+                    var dateFormat = myDateRangeTarget.datepicker("option", "dateFormat");
+                    var dateRange = compareDates(textStart, dateText, dateFormat);
+                    myDateRangeTarget.val(dateRange.start + options.rangeSeparator + dateRange.end);
+                    inst.rangeStart = null;
+                              if (options.useHiddenAltFields){
+                                  var myToField = myDateRangeTarget.attr("data-to-field");
+                                  var myFromField = myDateRangeTarget.attr("data-from-field");
+                                  $("#"+myFromField).val(dateRange.start);
+                                  $("#"+myToField).val(dateRange.end);
+                              }
+                  }
+                }
+                onSelect.apply(myDateRangeTarget, arguments);
+              };
+          
+              options.onClose = function(dateText, inst) {
+                inst.rangeStart = null;
+                inst.inline = false;
+                onClose.apply(myDateRangeTarget, arguments);
+              };
+          
+              return this.each(function() {
+                if (myDateRangeTarget.is("input")) {
+                  myDateRangeTarget.datepicker(options);
+                }
+                myDateRangeTarget.wrap("<div class=\"dateRangeWrapper\"></div>");
+              });
+            };
+          }(jQuery));
+
+        //AFTER CLICKING THE DATE RANGE ICON.
+        //This code will take the values and show the data for the selected date range
+
+        $("#dateRange_icon").click(function(){
+
+            $('#entry_log_tableBody').empty(); //emptying before appending data
+
+            console.log("Date range icon Clicked!");
+
+            //taking the value from the input fields
+            let startDate = $("#txtDateFrom").val();
+            let endDate = $("#txtDateTo").val();
+
+            console.log("startDate: "+startDate);
+            console.log("endDate: "+endDate);
+
+            // Returns an array of dates between the two dates
+            var getDates = function(startDate, endDate) {
+                var dates = [],
+                    currentDate = startDate,
+                    addDays = function(days) {
+                    var date = new Date(this.valueOf());
+                    date.setDate(date.getDate() + days);
+                    return date;
+                    };
+                    while (currentDate <= endDate) {
+                    dates.push(currentDate);
+                    currentDate = addDays.call(currentDate, 1);
+                    }
+                    return dates;
+            };
+
+            // Calling function getDates
+            //Function returns all the dates between the start date and end date
+            var dates = getDates(new Date(startDate), new Date(endDate));                                                                                                           
+            dates.forEach(function(date) {
+                let dd = date.getDate();
+                let mm = date.getMonth()+1;
+                let yyyy = date.getFullYear();
+
+                if(dd<10) {
+                    dd = '0'+dd
+                } 
+            
+                if(mm<10) {
+                    mm = '0'+mm
+                } 
+            
+                let finalDate = yyyy + '-' + mm + '-' + dd;
+                console.log(finalDate);
+
+                //Now take the dates and search for them in the datebase
+                //search in database
+                for (let uniqueKey in fetchedData){
+                    
+                    let issueDate = fetchedData[uniqueKey]['issueDate'];
+
+                    if (issueDate==finalDate){
+                        console.log("Date matched!! "+issueDate);
+
+                        //reversing the key value in the database so that the last entry shows up first
+                        let itemCode = fetchedData[uniqueKey]['itemCode'];
+                        let itemName = fetchedData[uniqueKey]['itemName'];
+                        let uom = fetchedData[uniqueKey]['uom'];
+                        let quantity = fetchedData[uniqueKey]['quantity'];
+                        let unitRate = fetchedData[uniqueKey]['unitRate'];
+                        let totalAmount = fetchedData[uniqueKey]['totalAmount'];
+                        let mainContract = fetchedData[uniqueKey]['mainContract'];
+                        let mainVendor = fetchedData[uniqueKey]['mainVendor'];
+                        let novatedContract = fetchedData[uniqueKey]['novatedContract'];
+                        let novatedVendor = fetchedData[uniqueKey]['novatedVendor'];
+                        let PRnum = fetchedData[uniqueKey]['PRnum'];
+                        let POnum = fetchedData[uniqueKey]['POnum'];
+                        let delChalNum = fetchedData[uniqueKey]['delChalNum'];
+                        let user_email = fetchedData[uniqueKey]['user_email'];
+                        let current_date = fetchedData[uniqueKey]['current_date'];
+
+
+                        let user_name = user_email.split("@"); //split the array so that you can display the name before the @ domain
+
+
+                        // appending elements into the databaseTable
+                        $('#entry_log_tableBody').append(/*html*/`
+                        <tr data-key="${uniqueKey}">
+                            <td>
+                                ${itemCode}
+                            </td>
+                            <td>
+                                ${itemName}
+                            </td>
+                            <td>
+                                ${uom}
+                            </td>
+                            <td>
+                                ${quantity}
+                            </td>
+                            <td>
+                                ${unitRate}
+                            </td>
+                            <td>
+                                ${totalAmount}
+                            </td>
+                            <td>
+                                ${mainContract}
+                            </td>
+                            <td>
+                                ${mainVendor}
+                            </td>
+                            <td>
+                                ${novatedContract}
+                            </td>
+                            <td>
+                                ${novatedVendor}
+                            </td>
+                            <td>
+                                ${PRnum}
+                            </td>
+                            <td>
+                                ${POnum}
+                            </td>
+                            <td>
+                                ${delChalNum}
+                            </td>
+                            <td>
+                                ${issueDate}
+                            </td>
+                            <td>
+                                ${user_name[0]}
+                            </td>
+                            <td>
+                                ${current_date}
+                            </td>
+                        </tr>
+                    `);
+                    }
+                }
+
+            });
+            
+
+        });
+          
+        $(document).ready(function(){
+              $("#txtDateRange").dateRangePicker({
+                  showOn: "focus",
+                  rangeSeparator: " to ",
+                  dateFormat: "mm-dd-yy",
+                  useHiddenAltFields: true,
+                  constrainInput: true
+              });
+        });
+
+        
+  
     });
 }
